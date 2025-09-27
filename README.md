e94383d5bbc6adfd1c99caed9170684b0d526125
Jacob Evans <Jevan234@students.kennesaw.edu>
2025-09-27 00:56:15 -0400

    changed main.py, added logos, and added menu background

--- README.md
+++ README.md
@@ -3,9 +3,7 @@ ka-rib-dis
 This is the active repository of the Fall 2025 KSU Paradox Game Jam.
 Screen size = 1280 x 960
 # Gameplay:
-The goal of this game is for you to play as Theseus, taking turns between combat and ship management. There will be 3 endings, one you complete the quest (repeat the loop/pardox remains), Sink the ship (break the loop), and plead with posidon to after the voyage. 
+The goal of this game is for you to play as Theseus, taking turns between combat and ship management.
 
 # Story:
-You were eternally bound to the ship of theseus, as long as it floats, you will be the captain. you're are un charge of supply runs to repair yourship you use
-
-# Endings:
+You were eternally bound to the ship of theseus, as long as it floats, you will be the captain. you're are in charge of supply runs to repair yourship you use
--- src/__main__.py
+++ src/__main__.py
@@ -10,16 +10,22 @@ running = True
 pg.display.set_caption("Charybdis")
 
 # Load assets
+BackgroundPath = "./assets/titlescreen-PLACEHOLDER-1280-960.png"
+Background = pg.image.load(BackgroundPath).convert_alpha()
 LogoPath = "./assets/LCKSULogo.png"
 LCKSULogo = pg.image.load(LogoPath).convert_alpha()
-LCKSULogo = pg.transform.scale(LCKSULogo, (500,500))
+LCKSULogo = pg.transform.scale(LCKSULogo, (200,200))
+PyGameLogoPath = "./assets/pygame_logo.png"
+PygameLogo = pg.image.load(PyGameLogoPath).convert_alpha()
+PygameLogo = pg.transform.scale(PygameLogo, (200,100))
+
 
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-    for i in range (100, 0,-10):
-        LCKSULogo.set_alpha(i)
-    screen.blit(LCKSULogo, (390,230))
+    screen.blit(Background, (0,0))
+    screen.blit(LCKSULogo, (20,720))
+    screen.blit(PygameLogo, (240,800))
     pg.display.flip()
     clock.tick(60)
