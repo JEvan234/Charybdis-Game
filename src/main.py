94d6b00afd2ca2b3aa0aa69379257a367d097750
Jacob Evans <Jevan234@students.kennesaw.edu>
2025-09-26 23:52:13 -0400

    moved all source scripts, updated main

--- src/main.py
+++ src/main.py
@@ -10,11 +10,17 @@ clock = pg.time.Clock()
 running = True
 pg.display.set_caption("Charybdis")
 
+# Load assets
+LogoPath = "./assets/LCKSULogo.png"
+LCKSULogo = pg.image.load(LogoPath).convert_alpha()
+LCKSULogo = pg.transform.scale(LCKSULogo, (500,500))
 
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-    screen.fill("black")
+    for i in range (100, 0,-10):
+        LCKSULogo.set_alpha(i)
+    screen.blit(LCKSULogo, (390,230))
     pg.display.flip()
     clock.tick(60)
